13298897d3077dfaaf1357e903d1097acdf0d1a3
MuneerBSK <muneerbsk96@gmail.com>
2024-05-05 18:27:16 -0400

    AutoPush

--- docdb.tf
+++ docdb.tf
@@ -1,11 +1,11 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "robot-${var.ENV}-docdb"
   engine                  = "docdb"
-  master_username         = admin1
-  master_password         = roboshop1
+  master_username         = local.DOCDB_USER
+  master_password         = local.DOCDB_PASS
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
-#   vpc_security_group_ids  = [aws_security_group.allow_mongodb.id]
+  vpc_security_group_ids  = [aws_security_group.allow_mongodb.id]
 }
 
 # Creates Subnet Group Needed to host the docdb cluster 
@@ -14,10 +14,17 @@ resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
   tags = {
-    Name = "robot-${var.ENV}-docdb-subnet-group"
+    Name = "robot-${var.ENV}-dodb-subnet-group"
   }
 }
 
-output "output_ref" {
-  value = data.terraform_remote_state.vpc
+# Creates Instances Needed for the DocDB Cluster
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = var.DOCDB_INSTANCE_COUNT
+  identifier         = "robot-${var.ENV}-docdb-instance"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = var.DOCDB_INSTANCE_CLASS
+    depends_on = [
+      aws_docdb_cluster.docdb
+    ]
 }
