05a2632f99ede5fdeba35e0356afae98df56b780
MuneerBSK <muneerbsk96@gmail.com>
2024-05-05 18:29:49 -0400

    AutoPush

--- docdb.tf
+++ docdb.tf
@@ -1,11 +1,11 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "robot-${var.ENV}-docdb"
   engine                  = "docdb"
-  master_username         = local.DOCDB_USER
-  master_password         = local.DOCDB_PASS
+  master_username         = admin1
+  master_password         = roboshop1
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
-  vpc_security_group_ids  = [aws_security_group.allow_mongodb.id]
+#   vpc_security_group_ids  = [aws_security_group.allow_mongodb.id]
 }
 
 # Creates Subnet Group Needed to host the docdb cluster 
@@ -14,7 +14,7 @@ resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
   tags = {
-    Name = "robot-${var.ENV}-dodb-subnet-group"
+    Name = "robot-${var.ENV}-docdb-subnet-group"
   }
 }
 
