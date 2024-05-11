2ec1fff715199ad09e72b5cab90162e1a56f0f65
MuneerBSK <muneerbsk96@gmail.com>
2024-05-11 18:24:43 -0400

    AutoPush

--- r53.tf
+++ r53.tf
@@ -1,6 +1,6 @@
 # This created the CNAME Record for MongoDB
 resource "aws_route53_record" "mongodb" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  zone_id = "Z02407882HMY5LE1XLJU5"
   name    = "mongodb-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
